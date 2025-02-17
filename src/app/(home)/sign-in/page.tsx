660593a8d2f37bbf62f62900dfdc0245fa7e4f67
Marvellousz <pranavmurali024@gmail.com>
2025-02-17 14:44:50 +0530

    fix: improved css styles of sign in button

--- src/app/(home)/sign-in/page.tsx
+++ src/app/(home)/sign-in/page.tsx
@@ -3,7 +3,14 @@ import { SignInButton } from "@clerk/nextjs";
 export default function HomePage() {
   return (
     <>
-      <SignInButton forceRedirectUrl={"/drive"} />
+      <SignInButton forceRedirectUrl={"/drive"}>
+        <button
+          type="submit"
+          className="border border-neutral-700 bg-neutral-800 text-white transition-colors hover:bg-neutral-700 px-4 py-2 text-lg rounded-md"
+        >
+          Sign In
+        </button>
+      </SignInButton>
       <footer className="mt-16 text-sm text-neutral-500">
         © {new Date().getFullYear()} Arkhive Drive. All rights reserved.
       </footer>
